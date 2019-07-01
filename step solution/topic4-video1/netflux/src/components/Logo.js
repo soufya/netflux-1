40416d16cedc086336ce7152383282dc8ba03ad4
Athoug Alsoughayer <athoug2.s@gmail.com>
2019-07-01 10:54:39 +0300

    default props

--- step solution/topic4-video1/netflux/src/components/Logo.js
+++ step solution/topic4-video1/netflux/src/components/Logo.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import logo from '../img/logo.svg';
+import React from "react";
+import logo from "../img/logo.svg";
 
 const Logo = (props) => {
   return (
-    <a className={`navbar-item ${props.classes}`} href="#/"><img src={logo} width="80" alt="netflux logo" /></a>
+    <a className={props.classes} href="#/"><img src={logo} width={props.width} alt="netflux logo" /></a>
   );
+}
+
+Logo.defaultProps = {
+  classes: "navbar-item",
+  width: 80
 }
 
 export default Logo;
