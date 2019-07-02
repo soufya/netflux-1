dbe35d543725692617de4680c872b82281c19091
Athoug Alsoughayer <athoug2.s@gmail.com>
2019-07-02 16:26:40 +0300

    topic5-video1 starter file

--- step solution/topic4-video3/netflux/src/components/NewShows.js
+++ step solution/topic4-video3/netflux/src/components/NewShows.js
@@ -5,6 +5,7 @@ import MovieGrid from "./MovieGrid";
 const NewShows = props => {
   const movies = props.movies.slice(0, 8);
   const movie = movies.map(data => <Movie image={data.posterPath} title={data.originalTitle} key={data.movieVideos.key} />);
+
   return (
     <MovieGrid gridType="is-movies" title="أحدث المسلسلات">
      { movie }
