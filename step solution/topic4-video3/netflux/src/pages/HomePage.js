5f10086780dba6334aeb65d388321b41ed3ff80c
Athoug Alsoughayer <athoug2.s@gmail.com>
2019-07-02 14:34:45 +0300

    rendered suggested movie list

--- step solution/topic4-video3/netflux/src/pages/HomePage.js
+++ step solution/topic4-video3/netflux/src/pages/HomePage.js
@@ -8,12 +8,6 @@ import NewShows from '../components/NewShows';
 import NewMovies from '../components/NewMovies';
 import Footer from '../components/Footer';
 
-// import images
-import image1 from "../img/movie1.svg";
-import image2 from "../img/movie2.svg";
-import image3 from "../img/movie3.svg";
-import image4 from "../img/movie4.svg";
-
 // import data
 import data from "../data/movies.json";
 
@@ -24,7 +18,7 @@ class HomePage extends React.Component {
   }
 
   SuggestedMovies() {
-    return this.state.movies.filter(movie => movie.voteAverage > 7);
+    return this.state.movies.filter(movie => movie.voteAverage > 8);
   }
 
   render() {
