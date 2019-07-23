e8a0a616daffd01a9096bd1fb1cbf3ed65fd0fb4
Athoug Alsoughayer <athoug2.s@gmail.com>
2019-07-23 15:48:31 +0300

    topic7-video4  step solution

---
+++ step solution/topic7-video4/netflux/src/App.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import Details from './pages/Details';
+import CategoryPage from './pages/CategoryPage';
+
+
+import data from "./data/movies.json"
+
+class App extends React.Component {
+  state = {
+    movies: data
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="wrapper">
+          <div className="container">
+            <Header />
+            <>
+              <Route
+                exact
+                path="/"
+                render={(props) => (<HomePage
+                  {...props}
+                  movies={this.state.movies}
+                />)}
+              />
+
+              <Route
+                exact
+                path="/movies"
+                render={(props) => (<CategoryPage
+                  {...props}
+                  type="Film"
+                  typeText="الأفلام"
+                  optionText="نوع الفيلم"
+                  movies={this.state.movies}
+                />)}
+              />
+
+              <Route
+                exact
+                path="/shows"
+                render={(props) => (<CategoryPage
+                  {...props}
+                  type="Film" /* TODO: change type to show later when data is there */
+                  typeText="المسلسلات"
+                  optionText="نوع المسلسل"
+                  movies={this.state.movies}
+                />)}
+              />
+
+            <Route
+              exact
+              path="/details/:id"
+              render={(props) => (<Details
+                {...props}
+                movies={this.state.movies}
+              />)}
+            />
+            </>
+            <Footer />
+          </div>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
---
+++ step solution/topic7-video4/netflux/src/components/Filter.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+class Filter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "" };
+    this.onChangeHandler = this.onChangeHandler.bind(this);
+  }
+
+  onChangeHandler(event) {
+    this.props.onSelectChange(event.target.value);
+    this.setState({ value: event.target.value });
+  }
+
+  render() {
+    return (
+      <section className="section filter">
+        <div className="container">
+          <form>
+            <label className="movieTypeLabel">{this.props.type}</label>
+            <select className="movieTypeSelect" onChange={this.onChangeHandler}>
+              <option value="">{this.props.optionText}</option>
+              <option value="Action">الأكشن</option>
+              <option value="Thriller">الغموض</option>
+              <option value="Drama">الدراما</option>
+              <option value="Comedy">الكوميدي</option>
+            </select>
+          </form>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Filter;
